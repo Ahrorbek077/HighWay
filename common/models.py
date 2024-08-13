9f5a37cda673e726f3e77601fe19c387831168e4
Jamolkhon <jamolxonahmedov124@gmail.com>
2024-08-13 16:15:18 +0500

    feat(common/models): Gallery model

--- common/models.py
+++ common/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Gallery(models.Model):
+    name = models.CharField("name", max_length=256)
+    description = models.CharField("description", max_length=256)
+    image = models.ImageField("image", upload_to="gallery/%Y/%m")
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        db_table = "gallery"
+        verbose_name = "gallery"
+        verbose_name_plural = "galleries"
