a207bee857dc1c3b8438ced67613cc1105628dd2
Jamolkhon <jamolxonahmedov124@gmail.com>
2024-08-13 16:39:18 +0500

    style(common/models): reordering

--- common/models.py
+++ common/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
+
 class Gallery(models.Model):
     name = models.CharField("name", max_length=256)
     description = models.CharField("description", max_length=256)
     image = models.ImageField("image", upload_to="gallery/%Y/%m")
-    def __str__(self):
-        return f"{self.name}"
 
     class Meta:
         db_table = "gallery"
         verbose_name = "gallery"
         verbose_name_plural = "galleries"
+
+    def __str__(self):
+        return f"{self.name}"
